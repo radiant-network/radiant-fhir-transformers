bfa133e0fcf44726400bea4cba30e920a1980c30
Charles Haynes <haynescd@chop.edu>
2025-04-10 11:27:52 -0400

    :sparkles: Add initial Observation Resource support

--- radiant_fhir_transform_cli/transform/classes/__init__.py
+++ radiant_fhir_transform_cli/transform/classes/__init__.py
@@ -2,9 +2,15 @@
 List of FHIR ndjson to dict transformers
 """
 
+from radiant_fhir_transform_cli.transform.classes.observation import (
+    ObservationTransformer,
+)
 from radiant_fhir_transform_cli.transform.classes.patient import (
     PatientTransformer,
 )
 
 # Map FHIR resource type to its transformer class
-transformers = {"Patient": PatientTransformer}
+transformers = {
+    "Patient": PatientTransformer,
+    "Observation": ObservationTransformer,
+}
