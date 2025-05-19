7c96d011b96ee3ee847fd00c3fe58aa89842f260
liberaliscomputing <liberaliscomputing@gmail.com>
2025-05-19 09:23:07 -0400

    :white_check_mark: Add tests for MedicationRequest transformer classes

---
+++ tests/data/medication_request/__init__.py
@@ -0,0 +1,48 @@
+from .medication_request import MedicationRequestTestHelper
+from .medication_request_identifier import MedicationRequestIdentifierTestHelper
+from .medication_request_status_reason_coding import (
+    MedicationRequestStatusReasonCodingTestHelper,
+)
+from .medication_request_category import MedicationRequestCategoryTestHelper
+from .medication_request_medication_codeable_concept_coding import (
+    MedicationRequestMedicationCodeableConceptCodingTestHelper,
+)
+from .medication_request_supporting_information import (
+    MedicationRequestSupportingInformationTestHelper,
+)
+from .medication_request_performer_type_coding import (
+    MedicationRequestPerformerTypeCodingTestHelper,
+)
+from .medication_request_reason_code import (
+    MedicationRequestReasonCodeTestHelper,
+)
+from .medication_request_reason_reference import (
+    MedicationRequestReasonReferenceTestHelper,
+)
+from .medication_request_instantiates_canonical import (
+    MedicationRequestInstantiatesCanonicalTestHelper,
+)
+from .medication_request_instantiates_uri import (
+    MedicationRequestInstantiatesUriTestHelper,
+)
+from .medication_request_based_on import MedicationRequestBasedOnTestHelper
+from .medication_request_course_of_therapy_type_coding import (
+    MedicationRequestCourseOfTherapyTypeCodingTestHelper,
+)
+from .medication_request_insurance import MedicationRequestInsuranceTestHelper
+from .medication_request_note import MedicationRequestNoteTestHelper
+from .medication_request_dosage_instruction import (
+    MedicationRequestDosageInstructionTestHelper,
+)
+from .medication_request_substitution_allowed_codeable_concept_conding import (
+    MedicationRequestSubstitutionAllowedCodeableConceptCodingTestHelper,
+)
+from .medication_request_substitution_reason_coding import (
+    MedicationRequestSubstitutionReasonCodingTestHelper,
+)
+from .medication_request_detected_issue import (
+    MedicationRequestDetectedIssueTestHelper,
+)
+from .medication_request_event_history import (
+    MedicationRequestEventHistoryTestHelper,
+)
