a7b15bf9d47dc0b950ca0cd6d1f1bb2f48229cb8
liberaliscomputing <liberaliscomputing@gmail.com>
2025-06-11 14:50:45 -0400

    :sparkles::white_check_mark: Add RelatedPerson photo transformer class and test

---
+++ tests/data/related_person/related_person_photo.py
@@ -0,0 +1,50 @@
+"""
+Test helper class for FHIR resource type RelatedPerson subtype Photo
+"""
+
+from radiant_fhir_transform_cli.transform.classes.related_person import (
+    RelatedPersonPhotoTransformer,
+)
+from tests.data.base import FhirResourceTestHelper
+
+from .related_person_resource import RESOURCE
+
+EXPECTED_OUTPUT = [
+    {
+        "related_person_id": "benedicte",
+        "photo_content_type": "image/jpeg",
+        "photo_language": None,
+        "photo_url": "Binary/f016",
+        "photo_size": None,
+        "photo_title": None,
+        "photo_creation": None,
+    },
+]
+
+
+class RelatedPersonPhotoTestHelper(FhirResourceTestHelper):
+    """
+    A helper class for testing transformations of the FHIR 'RelatedPerson' resource.
+
+    This class extends the FhirResourceTestHelper and is specifically
+    designed to assist in testing the transformation of the 'RelatedPerson' resource.
+
+    It predefines the resource type as 'RelatedPerson'
+    and initializes the resource with the specific 'RelatedPerson' resource payload
+    and its expected transformation output.
+
+    Attributes:
+        resource_type (str): The type of FHIR resource being tested, which
+          is set to 'RelatedPerson'.
+        resource (dict): The raw FHIR 'RelatedPerson' resource payload to be tested.
+        expected_output (dict): The expected transformation result of the
+          'RelatedPerson' resource payload.
+    """
+
+    resource_type = "RelatedPerson"
+    resource_subtype = "photo"
+    transformer = RelatedPersonPhotoTransformer
+    expected_table_name = "related_person_photo"
+
+    def __init__(self):
+        super().__init__(RESOURCE, EXPECTED_OUTPUT)
