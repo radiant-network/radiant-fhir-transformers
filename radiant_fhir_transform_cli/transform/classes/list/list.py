635fd91c82bfa6b83e924d27b411c85e68f093a9
liberaliscomputing <liberaliscomputing@gmail.com>
2025-06-10 23:02:23 -0400

    :sparkles::white_check_mark: Add List transformer class and test

---
+++ radiant_fhir_transform_cli/transform/classes/list/list.py
@@ -0,0 +1,115 @@
+"""
+FHIR List transformer
+"""
+
+from radiant_fhir_transform_cli.transform.classes.base import (
+    FhirResourceTransformer,
+)
+
+TRANSFORM_SCHEMA = [
+    # Id
+    {
+        "fhir_path": "id",
+        "columns": {
+            "id": {"fhir_key": "id", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "resourceType",
+        "columns": {
+            "resource_type": {"fhir_key": "resourceType", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "status",
+        "columns": {
+            "status": {"fhir_key": "status", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "mode",
+        "columns": {
+            "mode": {"fhir_key": "mode", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "title",
+        "columns": {
+            "title": {"fhir_key": "title", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "code",
+        "columns": {
+            "code_text": {"fhir_key": "text", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "subject",
+        "fhir_reference": "subject_reference",
+        "columns": {
+            "subject_reference": {"fhir_key": "reference", "type": "str"},
+            "subject_type": {"fhir_key": "type", "type": "str"},
+            "subject_display": {"fhir_key": "display", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "encounter",
+        "fhir_reference": "encounter_reference",
+        "columns": {
+            "encounter_reference": {"fhir_key": "reference", "type": "str"},
+            "encounter_type": {"fhir_key": "type", "type": "str"},
+            "encounter_display": {"fhir_key": "display", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "date",
+        "columns": {
+            "date": {"fhir_key": "date", "type": "datetime"},
+        },
+    },
+    {
+        "fhir_path": "source",
+        "fhir_reference": "source_reference",
+        "columns": {
+            "source_reference": {"fhir_key": "reference", "type": "str"},
+            "source_type": {"fhir_key": "type", "type": "str"},
+            "source_display": {"fhir_key": "display", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "orderedBy",
+        "columns": {
+            "ordered_by_text": {"fhir_key": "text", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "emptyReason",
+        "columns": {
+            "empty_reason_text": {"fhir_key": "text", "type": "str"},
+        },
+    },
+]
+
+
+class ListTransformer(FhirResourceTransformer):
+    """
+    A transformer class for the 'List' resource in FHIR.
+
+    Transform List JSON objects into flat dictionaries representing
+    rows in an output CSV file
+
+
+    Attributes:
+        resource_type (str): The type of FHIR resource being transformed
+        transform_schema (list[dict]): The transformation dictionary used to map
+          and transform the resource data
+
+    Methods:
+        __init__(self):
+            Initializes the ListTransformer instance with the resource
+            type 'List' and a transformation dictionary.
+    """
+
+    def __init__(self):
+        super().__init__("List", None, TRANSFORM_SCHEMA)
---
+++ tests/data/list/list.py
@@ -0,0 +1,61 @@
+"""
+Test helper class for FHIR resource type List
+"""
+
+from radiant_fhir_transform_cli.transform.classes.list import (
+    ListTransformer,
+)
+from tests.data.base import FhirResourceTestHelper
+
+from .list_resource import RESOURCE
+
+EXPECTED_OUTPUT = [
+    {
+        "id": "med-list",
+        "resource_type": "List",
+        "status": "current",
+        "mode": "changes",
+        "title": "Current Medication List",
+        "code_text": "Medication Review",
+        "subject_reference": "example",
+        "subject_type": None,
+        "subject_display": None,
+        "encounter_reference": "example",
+        "encounter_type": None,
+        "encounter_display": None,
+        "date": "2013-11-20T23:10:23+11:00",
+        "source_reference": "example",
+        "source_type": None,
+        "source_display": None,
+        "ordered_by_text": None,
+        "empty_reason_text": None,
+    }
+]
+
+
+class ListTestHelper(FhirResourceTestHelper):
+    """
+    A helper class for testing transformations of the FHIR 'List' resource.
+
+    This class extends the FhirResourceTestHelper and is specifically
+    designed to assist in testing the transformation of the 'List' resource.
+
+    It predefines the resource type as 'List'
+    and initializes the resource with the specific 'List' resource payload
+    and its expected transformation output.
+
+    Attributes:
+        resource_type (str): The type of FHIR resource being tested, which
+          is set to 'List'.
+        resource (dict): The raw FHIR 'List' resource payload to be tested.
+        expected_output (dict): The expected transformation result of the
+          'List' resource payload.
+    """
+
+    resource_type = "List"
+    resource_subtype = None
+    transformer = ListTransformer
+    expected_table_name = "list"
+
+    def __init__(self):
+        super().__init__(RESOURCE, EXPECTED_OUTPUT)
