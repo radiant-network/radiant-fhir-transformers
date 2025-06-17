00482c77cf7b76afc309c5df195616e599ffe7d1
liberaliscomputing <liberaliscomputing@gmail.com>
2025-06-17 11:25:13 -0400

    :sparkles::white_check_mark: Add Goal transformer class and test

---
+++ radiant_fhir_transform_cli/transform/classes/goal/goal.py
@@ -0,0 +1,112 @@
+"""
+FHIR Goal transformer
+"""
+
+from radiant_fhir_transform_cli.transform.classes.base import (
+    FhirResourceTransformer,
+)
+
+TRANSFORM_SCHEMA = [
+    # Id
+    {
+        "fhir_path": "id",
+        "columns": {
+            "id": {"fhir_key": "id", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "resourceType",
+        "columns": {
+            "resource_type": {"fhir_key": "resourceType", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "lifecycleStatus",
+        "columns": {
+            "lifecycle_status": {"fhir_key": "lifecycleStatus", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "achievementStatus",
+        "columns": {
+            "achievement_status_text": {"fhir_key": "text", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "priority",
+        "columns": {
+            "priority_text": {"fhir_key": "text", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "description",
+        "columns": {
+            "description_text": {"fhir_key": "text", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "subject",
+        "fhir_reference": "subject_reference",
+        "columns": {
+            "subject_reference": {"fhir_key": "reference", "type": "str"},
+            "subject_type": {"fhir_key": "type", "type": "str"},
+            "subject_display": {"fhir_key": "display", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "startDate",
+        "columns": {
+            "start_date": {"fhir_key": "startDate", "type": "datetime"},
+        },
+    },
+    {
+        "fhir_path": "startCodeableConcept",
+        "columns": {
+            "start_codeable_concept_text": {"fhir_key": "text", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "statusDate",
+        "columns": {
+            "status_date": {"fhir_key": "statusDate", "type": "datetime"},
+        },
+    },
+    {
+        "fhir_path": "statusReason",
+        "columns": {
+            "status_reason": {"fhir_key": "statusReason", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "expressedBy",
+        "fhir_reference": "expressed_by_reference",
+        "columns": {
+            "expressed_by_reference": {"fhir_key": "reference", "type": "str"},
+            "expressed_by_type": {"fhir_key": "type", "type": "str"},
+            "expressed_by_display": {"fhir_key": "display", "type": "str"},
+        },
+    },
+]
+
+
+class GoalTransformer(FhirResourceTransformer):
+    """
+    A transformer class for the 'Goal' resource in FHIR.
+
+    Transform Goal JSON objects into flat dictionaries representing
+    rows in an output CSV file
+
+
+    Attributes:
+        resource_type (str): The type of FHIR resource being transformed
+        transform_schema (list[dict]): The transformation dictionary used to map
+          and transform the resource data
+
+    Methods:
+        __init__(self):
+            Initializes the GoalTransformer instance with the resource
+            type 'Goal' and a transformation dictionary.
+    """
+
+    def __init__(self):
+        super().__init__("Goal", None, TRANSFORM_SCHEMA)
---
+++ tests/data/goal/goal.py
@@ -0,0 +1,59 @@
+"""
+Test helper class for FHIR resource type Goal
+"""
+
+from radiant_fhir_transform_cli.transform.classes.goal import (
+    GoalTransformer,
+)
+from tests.data.base import FhirResourceTestHelper
+
+from .goal_resource import RESOURCE
+
+EXPECTED_OUTPUT = [
+    {
+        "id": "example",
+        "resource_type": "Goal",
+        "lifecycle_status": "on-hold",
+        "achievement_status_text": "Achieved",
+        "priority_text": "high",
+        "description_text": "Target weight is 160 to 180 lbs.",
+        "subject_reference": "example",
+        "subject_type": None,
+        "subject_display": "Peter James Chalmers",
+        "start_date": "2015-04-05",
+        "start_codeable_concept_text": None,
+        "status_date": "2016-02-14",
+        "status_reason": "Patient wants to defer weight loss until after honeymoon.",
+        "expressed_by_reference": "example",
+        "expressed_by_type": None,
+        "expressed_by_display": "Peter James Chalmers",
+    }
+]
+
+
+class GoalTestHelper(FhirResourceTestHelper):
+    """
+    A helper class for testing transformations of the FHIR 'Goal' resource.
+
+    This class extends the FhirResourceTestHelper and is specifically
+    designed to assist in testing the transformation of the 'Goal' resource.
+
+    It predefines the resource type as 'Goal'
+    and initializes the resource with the specific 'Goal' resource payload
+    and its expected transformation output.
+
+    Attributes:
+        resource_type (str): The type of FHIR resource being tested, which
+          is set to 'Goal'.
+        resource (dict): The raw FHIR 'Goal' resource payload to be tested.
+        expected_output (dict): The expected transformation result of the
+          'Goal' resource payload.
+    """
+
+    resource_type = "Goal"
+    resource_subtype = None
+    transformer = GoalTransformer
+    expected_table_name = "goal"
+
+    def __init__(self):
+        super().__init__(RESOURCE, EXPECTED_OUTPUT)
