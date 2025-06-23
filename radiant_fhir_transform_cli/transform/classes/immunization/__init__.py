ce399f013bd72c9e3737db4c0f6ef1d40de9e9e6
liberaliscomputing <liberaliscomputing@gmail.com>
2025-06-23 15:52:14 -0400

    :sparkles::white_check_mark: Add Immunization transformer classes and tests imports

---
+++ radiant_fhir_transform_cli/transform/classes/immunization/__init__.py
@@ -0,0 +1,33 @@
+from .immunization import ImmunizationTransformer
+from .immunization_identifier import ImmunizationIdentifierTransformer
+from .immunization_status_reason_coding import (
+    ImmunizationStatusReasonCodingTransformer,
+)
+from .immunization_vaccine_code_coding import (
+    ImmunizationVaccineCodeCodingTransformer,
+)
+from .immunization_report_origin_coding import (
+    ImmunizationReportOriginCodingTransformer,
+)
+from .immunization_site_coding import ImmunizationSiteCodingTransformer
+from .immunization_route_coding import ImmunizationRouteCodingTransformer
+from .immunization_performer import ImmunizationPerformerTransformer
+from .immunization_note import ImmunizationNoteTransformer
+from .immunization_reason_code import ImmunizationReasonCodeTransformer
+from .immunization_reason_reference import (
+    ImmunizationReasonReferenceTransformer,
+)
+from .immunization_subpotent_reason import (
+    ImmunizationSubpotentReasonTransformer,
+)
+from .immunization_education import ImmunizationEducationTransformer
+from .immunization_program_eligibility import (
+    ImmunizationProgramEligibilityTransformer,
+)
+from .immunization_funding_source_coding import (
+    ImmunizationFundingSourceCodingTransformer,
+)
+from .immunization_reaction import ImmunizationReactionTransformer
+from .immunization_protocol_applied import (
+    ImmunizationProtocolAppliedTransformer,
+)
