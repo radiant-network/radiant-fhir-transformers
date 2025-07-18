d9d2136a874758ca1779fd7de5d6e58918108e3f
liberaliscomputing <liberaliscomputing@gmail.com>
2025-07-18 11:47:42 -0400

    :sparkles::white_check_mark: Add missing subelements to Observation note transformer class and test

--- radiant_fhir_transform_cli/transform/classes/observation/observation_note.py
+++ radiant_fhir_transform_cli/transform/classes/observation/observation_note.py
@@ -7,6 +7,7 @@ from radiant_fhir_transform_cli.transform.classes.base import (
 )
 
 TRANSFORM_SCHEMA = [
+    # Primary Key
     {
         "fhir_path": None,
         "columns": {
@@ -22,12 +23,23 @@ TRANSFORM_SCHEMA = [
         },
     },
     {
-        "fhir_path": "note.text",
+        "fhir_path": "note",
         "columns": {
-            "note_text": {
-                "fhir_key": "note.text",
+            "note_author_reference_reference": {
+                "fhir_key": "authorReference.reference",
                 "type": "str",
             },
+            "note_author_reference_type": {
+                "fhir_key": "authorReference.type",
+                "type": "str",
+            },
+            "note_author_reference_display": {
+                "fhir_key": "authorReference.display",
+                "type": "str",
+            },
+            "note_author_string": {"fhir_key": "authorString", "type": "str"},
+            "note_time": {"fhir_key": "time", "type": "datetime"},
+            "note_text": {"fhir_key": "text", "type": "str"},
         },
     },
 ]
@@ -38,7 +50,7 @@ class ObservationNoteTransformer(FhirResourceTransformer):
     Transformer class for the 'Observation' resource in FHIR, focusing on the 'note' element.
 
     This class transforms FHIR Observation JSON objects into flat dictionaries suitable for CSV output,
-    extracting and processing information from the 'extension' field.
+    extracting and processing information from the 'note' field.
 
     Attributes:
         resource_type (str): The type of FHIR resource being transformed ('Observation').
@@ -47,7 +59,7 @@ class ObservationNoteTransformer(FhirResourceTransformer):
 
     Methods:
         __init__():
-            Initializes the ObservationCategoryCodingTransformer instance with the resource type 'Observation',
+            Initializes the ObservationNoteTransformer instance with the resource type 'Observation',
             subtype 'note', and the specified transformation dictionary.
     """
 
--- tests/data/observation/observation_note.py
+++ tests/data/observation/observation_note.py
@@ -1,5 +1,5 @@
 """
-Test helper class for FHIR resource type Observation subtype notes
+Test helper class for FHIR resource type Observation subtype Note
 """
 
 from radiant_fhir_transform_cli.transform.classes.observation import (
@@ -12,10 +12,20 @@ from .observation_resource import RESOURCE
 EXPECTED_OUTPUT = [
     {
         "observation_id": "fUru66DnsInJJFSK0eHsjU8K8GtyH6pkh0LeyaSldORw4",
+        "note_author_reference_reference": None,
+        "note_author_reference_type": None,
+        "note_author_reference_display": None,
+        "note_author_string": None,
+        "note_time": None,
         "note_text": """This test was developed and its performance characteristics determined by the Children's Hospital of Philadelphia clinical laboratory. The U. S. Food and Drug Administration has not approved or cleared this test; however, FDA clearance or approval is not currently required for clinical use. The results are not intended to be used as the sole means of clinical diagnosis or patient management decisions. This laboratory is certified under the Clinical Laboratory Improvement Amendments of 1988 (CLIA-88) as qualified to perform high complexity clinical laboratory testing.\r\nThe 2019 novel coronavirus (SARS-CoV-2) target nucleic acids NOT DETECTED by real-time PCR.\r\n\r\nThis test was developed and its performance characteristics determined by Cepheid. This test has not been FDA cleared or approved. This test has been authorized by the FDA under an Emergency Use Authorization (EUA). This test is only authorized for the duration of time that circumstances exist justifying the authorization of the emergency use of in vitro diagnostic tests for detection of SARS-CoV-2 virus and/or diagnosis of COVID-19 infection under section 564(b)(1) of the Act, 21 U.S.C. 360bbb-3(b)(1), unless the authorization is terminated or revoked sooner. The Children's Hospital of Philadelphia is authorized to perform this test as certified under the Clinical Laboratory Improvement Amendments of 1988 (CLIA-88) as qualified to perform high complexity clinical laboratory testing. The results are not intended to be used as the sole means of clinical diagnosis or patient management decision.""",
     },
     {
         "observation_id": "fUru66DnsInJJFSK0eHsjU8K8GtyH6pkh0LeyaSldORw4",
+        "note_author_reference_reference": None,
+        "note_author_reference_type": None,
+        "note_author_reference_display": None,
+        "note_author_string": None,
+        "note_time": None,
         "note_text": "Test CHOP",
     },
 ]
@@ -35,15 +45,13 @@ class ObservationNoteTestHelper(FhirResourceTestHelper):
     Attributes:
         resource_type (str): The type of FHIR resource being tested, which
           is set to 'Observation'.
-
         resource (dict): The raw FHIR 'Observation' resource payload to be tested.
-
         expected_output (dict): The expected transformation result of the
           'Observation' resource payload.
     """
 
     resource_type = "Observation"
-    resource_subtype = "extension"
+    resource_subtype = "note"
     transformer = ObservationNoteTransformer
     expected_table_name = "observation_note"
 
