6866f713e675f6aaad8d4693876af0f02fae960d
liberaliscomputing <liberaliscomputing@gmail.com>
2025-07-18 11:56:51 -0400

    :sparkles::white_check_mark: Add Observation partOf transformer class and test

---
+++ radiant_fhir_transform_cli/transform/classes/observation/observation_part_of.py
@@ -0,0 +1,56 @@
+"""
+FHIR Observation PartOf transformer
+"""
+
+from radiant_fhir_transform_cli.transform.classes.base import (
+    FhirResourceTransformer,
+)
+
+TRANSFORM_SCHEMA = [
+    # Primary Key
+    {
+        "fhir_path": None,
+        "columns": {
+            "id": {"fhir_key": None, "type": "str"},
+        },
+    },
+    # Foreign Key
+    {
+        "fhir_path": "id",
+        "is_foreign_key": True,
+        "columns": {
+            "observation_id": {"fhir_key": "id", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "partOf",
+        "fhir_reference": "part_of_reference",
+        "columns": {
+            "part_of_reference": {"fhir_key": "reference", "type": "str"},
+            "part_of_type": {"fhir_key": "type", "type": "str"},
+            "part_of_display": {"fhir_key": "display", "type": "str"},
+        },
+    },
+]
+
+
+class ObservationPartOfTransformer(FhirResourceTransformer):
+    """
+    Transformer class for the 'Observation' resource in FHIR, focusing on the 'partOf' element.
+
+    This class transforms FHIR Observation JSON objects into flat dictionaries suitable for CSV output,
+    extracting and processing information from the 'partOf' field.
+
+    Attributes:
+        resource_type (str): The type of FHIR resource being transformed ('Observation').
+        subtype (str): Specifies the sub-element of the resource to focus on ('part_of').
+        transform_dict (dict): A dictionary defining the mapping and transformation rules for the resource data.
+
+    Methods:
+        __init__():
+            Initializes the ObservationPartOfTransformer instance with the resource type 'Observation',
+            subtype 'part_of', and the specified transformation dictionary.
+    """
+
+    def __init__(self):
+        super().__init__("Observation", "part_of", TRANSFORM_SCHEMA)
