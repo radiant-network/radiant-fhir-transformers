b31a76d96b4616609f62c747586a25c62e721cec
Amanda Warkow <warkowa@chop.edu>
2025-11-12 09:06:09 -0500

    ✨ add substitution type transformer and tests

---
+++ radiant_fhir_transform_cli/transform/classes/medication_dispense/medication_dispense_substitution_type_coding.py
@@ -0,0 +1,63 @@
+"""
+FHIR MedicationDispense Substitution Type Coding transformer
+"""
+
+from radiant_fhir_transform_cli.transform.classes.base import (
+    FhirResourceTransformer,
+)
+
+TRANSFORM_SCHEMA = [
+    # Primary Key
+    {
+        "fhir_path": None,
+        "columns": {
+            "id": {"fhir_key": None, "type": "str"},
+        },
+    },
+    # Foreign Key
+    {
+        "fhir_path": "id",
+        "is_foreign_key": True,
+        "columns": {
+            "medication_dispense_id": {"fhir_key": "id", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "substitution.type.coding",
+        "columns": {
+            "substitution_type_coding_system": {
+                "fhir_key": "system",
+                "type": "str",
+            },
+            "substitution_type_coding_code": {"fhir_key": "code", "type": "str"},
+            "substitution_type_coding_display": {
+                "fhir_key": "display",
+                "type": "str",
+            },
+        },
+    },
+]
+
+
+class MedicationDispenseSubstitutionTypeCodingTransformer(FhirResourceTransformer):
+    """
+    A transformer class for the 'MedicationDispense' resource in FHIR, focusing on the 'substitution.type.coding' element.
+    
+    This class transforms FHIR MedicationDispense JSON objects into flat dictionaries suitable for CSV output,
+    extracting and processing information from the 'substitution.type.coding' field.
+
+    Attributes:
+        resource_type (str): The type of FHIR resource being transformed ('MedicationDispense').
+        subtype (str): Specifies the sub-element of the resource to focus on ('substitution_type_coding').
+        transform_dict (dict): A dictionary defining the mapping and transformation rules for the resource data.
+
+    Methods:
+        __init__():
+            Initializes the MedicationDispenseSubstitutionTypeCodingTransformer instance with the resource type 'MedicationDispense',
+            subtype 'substitution_type_coding', and the specified transformation dictionary.
+    """
+
+    def __init__(self):
+        super().__init__(
+            "MedicationDispense", "substitution_type_coding", TRANSFORM_SCHEMA
+        )
