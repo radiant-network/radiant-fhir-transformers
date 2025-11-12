29a5ea89002a7d9d7fef092f666190405bcdb745
Amanda Warkow <warkowa@chop.edu>
2025-11-12 09:04:15 -0500

    ✨ add detected issue transformer and tests

---
+++ radiant_fhir_transform_cli/transform/classes/medication_dispense/medication_dispense_detected_issue.py
@@ -0,0 +1,55 @@
+""""
+FHIR MedicationDispense DetectedIssue transformer
+"""
+
+
+from radiant_fhir_transform_cli.transform.classes.base import (
+    FhirResourceTransformer,
+)
+
+TRANSFORM_SCHEMA = [
+    # Primary Key
+    {
+        "fhir_path": None,
+        "columns": {
+            "id": {"fhir_key": None, "type": "str"},
+        },
+    },
+    # Foreign Key
+    {
+        "fhir_path": "id",
+        "is_foreign_key": True,
+        "columns": {
+            "medication_dispense_id": {"fhir_key": "id", "type": "str"},
+        },
+    },
+    {
+        "fhir_path": "detectedIssue",
+        "columns": {
+            "detected_issue_reference": {"fhir_key": "reference", "type": "str"},
+            "detected_issue_type": {"fhir_key": "type", "type": "str"},
+            "detected_issue_display": {"fhir_key": "display", "type": "str"},
+        },
+    },
+]
+
+class MedicationDispenseDetectedIssueTransformer(FhirResourceTransformer):
+    """
+    A transformer class for the 'MedicationDispense' resource in FHIR, focusing on the 'detectedIssue' element.
+
+    This class transforms FHIR MedicationDispense JSON objects into flat dictionaries suitable for CSV output,
+    extracting and processing information from the 'detectedIssue' field.
+
+    Attributes:
+        resource_type (str): The type of FHIR resource being transformed ('MedicationDispense').
+        subtype (str): Specifies the sub-element of the resource to focus on ('detected_issue').
+        transform_dict (dict): A dictionary defining the mapping and transformation rules for the resource data.
+
+    Methods:
+        __init__():
+            Initializes the MedicationDispenseDetectedIssueTransformer instance with the resource type 'MedicationDispense',
+            subtype 'detected_issue', and the specified transformation dictionary.
+    """
+
+    def __init__(self):
+        super().__init__("MedicationDispense", "detected_issue", TRANSFORM_SCHEMA)
