16a9b030c2646de172d1c7e028f5b5bf4bde0e1c
Charles Haynes <haynescd@chop.edu>
2025-11-19 17:41:50 -0500

    :bug: Fix Raw Transformer to allow multiple resourceTypes

--- radiant_fhir_transform_cli/transform/classes/raw_fhir.py
+++ radiant_fhir_transform_cli/transform/classes/raw_fhir.py
@@ -1,8 +1,10 @@
+from typing import Any, override
+
 from .base import FhirResourceTransformer
 
 VIEW_DEFINITION = {
     "resourceType": "ViewDefinition",
-    "resource": "Observation",
+    "resource": None,
     "select": [
         {
             "column": [
@@ -18,3 +20,11 @@ VIEW_DEFINITION = {
 class RawFhirResourceTransformer(FhirResourceTransformer):
     def __init__(self):
         super().__init__("fhir_resource", None, VIEW_DEFINITION)
+
+    @override
+    def transform_resource(
+        self, resource_idx: int, resource_dict: dict[str, Any]
+    ) -> list[dict[str, Any]]:
+        resource_type = resource_dict["resourceType"]
+        self.view_definition["resource"] = resource_type
+        return super().transform_resource(resource_idx, resource_dict)
