4a29bd40bccc5f4ba92c630c0fa80a5b5b8cc717
liberaliscomputing <liberaliscomputing@gmail.com>
2026-04-26 01:09:24 -0400

    :sparkles: Apply controlled nested handling to BodyStructure view definitions

--- radiant_fhir_transform_cli/transform/classes/body_structure/body_structure_location_qualifier.py
+++ radiant_fhir_transform_cli/transform/classes/body_structure/body_structure_location_qualifier.py
@@ -34,21 +34,15 @@ VIEW_DEFINITION = {
             "forEachOrNull": "locationQualifier",
             "column": [
                 {
+                    "name": "location_qualifier_coding",
+                    "path": "coding",
+                    "type": "string",
+                    "collection": True,
+                },
+                {
                     "name": "location_qualifier_text",
                     "path": "text",
                     "type": "string",
-                },
-            ],
-            "select": [
-                {
-                    "forEachOrNull": "coding",
-                    "column": [
-                        {
-                            "name": "location_qualifier_coding",
-                            "path": "$this",
-                            "type": "string",
-                        },
-                    ],
                 },
             ],
         },
